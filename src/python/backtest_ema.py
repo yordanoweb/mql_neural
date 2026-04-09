08b8c2b3abb677ddf4e40ed11b25171e89e05abc
yordanoweb <yordano@archcraft.local>
2026-04-09 10:55:52 -0300

    add optimization and rename some params

--- src/python/backtest_ema.py
+++ src/python/backtest_ema.py
@@ -7,19 +7,21 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser(description='Dual EMA Crossover Strategy - Long Only')
     parser.add_argument('--csv', type=str, default="ndx100_rates_h1_2020.csv", help='CSV file path')
-    parser.add_argument('--ema-fast', type=int, default=20, help='Fast EMA period')
+    parser.add_argument('--ema-period', type=int, default=20, help='Fast EMA period')
     parser.add_argument('--trade-start-hour', type=int, default=14, help='Start hour for trading (0-23)')
     parser.add_argument('--trade-end-hour', type=int, default=22, help='End hour for trading (0-23)')
     parser.add_argument('--cooldown', type=int, default=120, help='Cooldown period in minutes')
     parser.add_argument('--exit_percent', type=int, default=1, help='Price increase/decrease to close')
     parser.add_argument('--debug', action='store_true', default=False, help='Debug mode')
+    parser.add_argument('--optimize', action='store_true', default=False, help='Execute optimization')
+    parser.add_argument('--opt_params', type=str, default='Equity Final [$]', help='Parameters to optimize')
     return parser.parse_args()
 
 args = parse_args()
 CSV_FILE = args.csv
 
 class SgradtStrategy(Strategy):
-    ema_fast = args.ema_fast
+    ema_period = args.ema_period
     trade_start_hour = args.trade_start_hour
     trade_end_hour = args.trade_end_hour
     cooldown_mins = args.cooldown
@@ -29,7 +31,7 @@ class SgradtStrategy(Strategy):
     
     def init(self):
         close = self.data.Close
-        self.ema_fast_line = self.I(talib.EMA, close, self.ema_fast)
+        self.ema_line = self.I(talib.EMA, close, self.ema_period)
         self.entry_pattern = []
         self.last_exit_time = None
         self.entry_direction = None  # Track if we entered long (1) or short (-1)
@@ -38,8 +40,8 @@ class SgradtStrategy(Strategy):
         current_time = self.data.index[-1]
         
         # Record trend direction based on EMA slope
-        ema_rising = self.ema_fast_line[-1] > self.ema_fast_line[-2]
-        ema_falling = self.ema_fast_line[-1] < self.ema_fast_line[-2]
+        ema_rising = self.ema_line[-1] > self.ema_line[-2]
+        ema_falling = self.ema_line[-1] < self.ema_line[-2]
         
         if ema_rising:
             self.entry_pattern.append(1)
@@ -111,4 +113,10 @@ if __name__ == "__main__":
     run_stats = bt.run()
     print(run_stats)
 
+    if args.optimize:
+        optim = bt.optimize(ema_fast=range(6, 50, 1), maximize='Equity Final [$]')
+        print("OPTIMIZATION RESULT:")
+        print(optim)
+
     bt.plot(resample=False)
+
